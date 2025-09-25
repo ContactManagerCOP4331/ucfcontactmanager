48e3a5d88449e08243eb0f23f7271ee7d926ba80
amersamman <30806381+amersamman@users.noreply.github.com>
2025-09-25 13:23:30 -0400

    php tweak

--- LAMPAPI/SearchContact.php
+++ LAMPAPI/SearchContact.php
@@ -1,70 +1,77 @@
 <?php
+<?php
+    $inData = getRequestInfo();
 
-	$inData = getRequestInfo();
+    $searchResults = "";
+    $searchCount = 0;
 
-	$searchResults = "";
-	$searchCount = 0;
+    $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+    if ($conn->connect_error)
+    {
+        returnWithError( $conn->connect_error );
+    }
+    else
+    {
+        // validate userId
+        if (!isset($inData["userId"]) || intval($inData["userId"]) < 1) {
+            returnWithError("Invalid userId");
+            $conn->close();
+            exit();
+        }
 
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-	if ($conn->connect_error)
-	{
-		returnWithError( $conn->connect_error );
-	}
-	else
-	{
-		$stmt = $conn->prepare("SELECT * FROM Contacts WHERE (FirstName like ? OR LastName like?) AND UserID=?");
-		$colorName = "%" . $inData["search"] . "%";
-		$stmt->bind_param("sss", $colorName, $colorName, $inData["userId"]);
-		$stmt->execute();
+        // build LIKE pattern and ensure correct bind types: two strings and one integer
+        $stmt = $conn->prepare("SELECT * FROM Contacts WHERE (FirstName LIKE ? OR LastName LIKE ?) AND UserID = ?");
+        $colorName = "%" . $inData["search"] . "%";
+        $userIdInt = intval($inData["userId"]);
+        $stmt->bind_param("ssi", $colorName, $colorName, $userIdInt);
+        $stmt->execute();
 
-		$result = $stmt->get_result();
+        $result = $stmt->get_result();
 
-		while($row = $result->fetch_assoc())
-		{
-			if( $searchCount > 0 )
-			{
-				$searchResults .= ",";
-			}
-			$searchCount++;
-			// $searchResults .= '"' . $row["FirstName"] . '"';
-			//"." means add
-			$searchResults .= '{"FirstName" : "' . $row["FirstName"]. '", "LastName" : "' . $row["LastName"]. '", "Phone" : "' . $row["Phone"]. '", "Email" : "' . $row["Email"]. '", "UserID" : "' . $row["UserID"].'", "ID" : "' . $row["ID"]. '"}';
-		}
+        while($row = $result->fetch_assoc())
+        {
+            if( $searchCount > 0 )
+            {
+                $searchResults .= ",";
+            }
+            $searchCount++;
+            $searchResults .= '{"FirstName" : "' . $row["FirstName"]. '", "LastName" : "' . $row["LastName"]. '", "Phone" : "' . $row["Phone"]. '", "Email" : "' . $row["Email"]. '", "UserID" : "' . $row["UserID"].'", "ID" : "' . $row["ID"]. '"}';
+        }
 
-		if( $searchCount == 0 )
-		{
-			returnWithError( "No Records Found" );
-		}
-		else
-		{
-			returnWithInfo( $searchResults );
-		}
+        if( $searchCount == 0 )
+        {
+            returnWithError( "No Records Found" );
+        }
+        else
+        {
+            returnWithInfo( $searchResults );
+        }
 
-		$stmt->close();
-		$conn->close();
-	}
+        $stmt->close();
+        $conn->close();
+    }
 
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
+    function sendResultInfoAsJson( $obj )
+    {
+        header('Content-type: application/json');
+        echo $obj;
+    }
 
-	function returnWithError( $err )
-	{
-		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
+    function returnWithError( $err )
+    {
+        $retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+        sendResultInfoAsJson( $retValue );
+    }
 
-	function returnWithInfo( $searchResults )
-	{
-		$retValue = '{"results":[' . $searchResults . '],"error":""}';
-		sendResultInfoAsJson( $retValue );
-	}
+    function returnWithInfo( $searchResults )
+    {
+        $retValue = '{"results":[' . $searchResults . '],"error":""}';
+        sendResultInfoAsJson( $retValue );
+    }
 
 ?>
